93131af9a4e3ab1ed72d351f1af7449008ebe9b6
xhan-ri <xhan@rhapsody.com>
2016-03-01 20:00:32 -0800

    Add start activity & end activity.

---
+++ app/src/main/java/com/rhapsody/xhan/activitytransitiondemo/EndActivity.java
@@ -0,0 +1,32 @@
+package com.rhapsody.xhan.activitytransitiondemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class EndActivity extends AppCompatActivity {
+
+	ImageView imageView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_end);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		imageView = (ImageView)findViewById(R.id.id_image);
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				finish();
+			}
+		});
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+}
--- app/src/main/java/com/rhapsody/xhan/activitytransitiondemo/StartActivity.java
+++ app/src/main/java/com/rhapsody/xhan/activitytransitiondemo/StartActivity.java
@@ -1,5 +1,6 @@
 package com.rhapsody.xhan.activitytransitiondemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,22 +9,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
 
+	ImageView imageView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-
+		imageView = (ImageView)findViewById(R.id.id_image);
 		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+				startActivity(new Intent(StartActivity.this, EndActivity.class));
 			}
 		});
 	}
